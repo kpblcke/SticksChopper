e3b39474858897caf3271e26788cd63372ba949a
kpblcke <kpblcke@gmail.com>
2021-03-02 00:36:07 +0400

    Initial commit

---
+++ Assets/Scripts/Axe.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    [SerializeField] 
+    private Paddle woodCutter;
+
+    [SerializeField] 
+    private Vector3 offset = new Vector2(1.2f, 1.23f);
+
+    [SerializeField]
+    private Vector2 throwStrength = new Vector2(2f, 13f);
+    private bool hasStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockAxeToWoodCutter();
+            LaunchOnMouseClick();   
+        }
+    }
+
+    private void LockAxeToWoodCutter()
+    {
+        transform.position = woodCutter.transform.position + offset;
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = throwStrength;
+            hasStarted = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] 
+    private float screenWidthInUnits = 16;
+    [SerializeField]
+    private float minX = 1;
+    [SerializeField] 
+    private float maxX = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
+        transform.position = paddlePosition;
+    }
+}
